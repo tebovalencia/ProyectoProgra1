e1c734e74d478a3d1aa4d100c8f6c0e5e04781ba
tebovalencia <evalenciamonico@gmail.com>
2024-06-02 01:27:33 -0600

    actualizacion

--- ProyectoProgra1/main.cpp
+++ ProyectoProgra1/main.cpp
@@ -8,18 +8,20 @@
 
 using namespace std;
 
-int pantalla[100][5]; //1-Posicion X, 2-Posicion Y, 3-Tecla, 4-Color, 5-Simbolo
-int X=0,Y=0,i=0,j=0, p=0; //P se usa para contador de pantalla
+int pantalla[100][6]; //1-Posicion X, 2-Posicion Y, 3-Tecla, 4-Color, 5-Simbolo, 6-tamano
+int X=0,Y=0,i=0,j=0, p=0,n=0; //P se usa para contador de pantalla
 int tecla;
+int color=0;
 
 void gotoxy(int x, int y);
 COORD GetConsoleCursorPosition(HANDLE hConsoleOutput);
 void posicionCursor(int x, int y);
 void figuras(int F);
-void operadorPantalla (int tecla, int X, int Y);
-void operadores(char tecla);
+void operadorPantalla (int tecla, int X, int Y, int n, int color);
+void operadores(char tecla,int n);
 void imprimirPantalla();
 void menu(int X, int Y);
+void colorTexto(int color);
 
 int main()
 {
@@ -57,7 +59,7 @@ int main()
 
                         }
                 }
-                operadores(tecla);
+                operadores(tecla,0);
                 if (tecla == 27) { // Si la tecla es ESC
                     break; // Salir del bucle
                 }
@@ -99,7 +101,7 @@ void posicionCursor(int x, int y)
     COORD cursorPos = GetConsoleCursorPosition(hConsole);
 
     if (cursorPos.X==0 && x==-1 ){
-        a=103;
+        a=102;
 
     }
     else
@@ -111,11 +113,11 @@ void posicionCursor(int x, int y)
     else
     b=cursorPos.Y+y;
 
-    if (a>103){
+    if (a>102){
         a=a-103;
     }
     if (b>34){
-        b=b-34;
+        b=b-35;
     }
 
     gotoxy(a,b);
@@ -123,46 +125,60 @@ void posicionCursor(int x, int y)
 }
 
 //Funcion para imprimir las figuras deseadas
-void figuras(int F)
+void figuras(int F, int n)
 {
     int i,j,o;
 
     switch(F)
     {
     case 1:
-             for (i=1; i<=4 ; i++)
+            //Cuadrado
+             for (i=1; i<=n/2 ; i++)
             {
-             for (j=0;j<=4;j++)
+             for (j=0;j<=n;j++)
              {
+                 posicionCursor(1,0);
                  cout<<".";
+                 posicionCursor(-1,0);
+
              }
-             posicionCursor(-j,1);
+             for(o=0;o<=n;o++){
+             posicionCursor(-1,0);
+             }
+             posicionCursor(0,1);
             }
             break;
 
     case 2:
-            for (i=1; i<=8;)
+            //Triangulo
+            n=n+2;
+            for (i=1; i<=n;)
             {
                 for (j=1; j<=i; j++)
                 {
+                    posicionCursor(1,0);
                     cout<<".";
+                    posicionCursor(-1,0);
                 }
-                posicionCursor(-i-1,1);
 
+                for(o=0;o<=i;o++){
+                     posicionCursor(-1,0);
+                     }
+                     posicionCursor(0,1);
                 i=i+2;
             }
             break;
 
     case 3:
-            for (i=1;i<=4;i++)
+            //Linea
+            for (i=1;i<=n;i++)
             {
-                for(j=4;j>=1;)
-                {
+                posicionCursor(1,0);
                     cout<<".";
-                    j=j-i;
-                }
+                posicionCursor(-1,0);
             }
             break;
+
     default: cout<<"Ninguna figura seleccionada";break;
 
     }
@@ -171,11 +187,14 @@ void figuras(int F)
 
 
 //Con esta funcion guardare la posicion de todas las figuras en pantalla para luego reimprimirlas para limpiar el menu
-void operadorPantalla (int tecla, int X, int Y){
+void operadorPantalla (int tecla, int X, int Y, int n, int color){
 
     pantalla[p][1]=X;
     pantalla[p][2]=Y;
     pantalla[p][3]=tecla;
+    pantalla[p][4]=color;
+    pantalla[p][5]=0;
+    pantalla[p][6]=n;
     imprimirPantalla();
 
 }
@@ -185,12 +204,13 @@ void imprimirPantalla()
     system("cls");
     for (j=0;j<=100;j++){
     gotoxy(pantalla[j][1],pantalla[j][2]);
-    operadores(pantalla[j][3]);
+    colorTexto(pantalla[j][4]);
+    operadores(pantalla[j][3],pantalla[j][6]);
     }
 }
 
 //Operadores de seleccion de tecla
-void operadores(char tecla)
+void operadores(char tecla,int n)
 {
     switch (tecla)
     {
@@ -199,23 +219,80 @@ void operadores(char tecla)
         case 72: posicionCursor(0,-1);break;
         case 80: posicionCursor(0,1);break;
         case 75: posicionCursor(-1,0);break;
-        case 59: figuras(1);break; //Triangulo F1
-        case 60: figuras(2);break; //Cuadrado F2
-        case 61: figuras(3);break; //Circulo
+        case 59: figuras(1,n);break; //Triangulo F1
+        case 60: figuras(2,n);break; //Cuadrado F2
+        case 61: figuras(3,n);break; //Circulo
     }
 }
 
 //Funcion de menu
 void menu(int X, int Y){
+
+    int i=0;
+
+    reiniciar:
+    system("cls");
     gotoxy(50,0);cout<<"Menu"<<endl;
-    gotoxy(30,1);cout<<"Esc: Salir,   F1:Cuadrado  F2:Triangulo F3:Circulo";
+    gotoxy(20,1);cout<<"Esc: Salir,   F1:Cuadrado    F2:Triangulo   F3:Linea   p:Cambiar color";
     tecla = _getch();
+
     if (tecla == 0 || tecla == 224) { // Teclas extendidas
-        tecla = _getch();}
-    operadorPantalla(tecla,X,Y);
+        tecla = _getch();
+
+        if (tecla==66){//F8
+        system("cls");
+        cout<<"Ingrese el color que desea: \n";
+        cout<<"1- Azul oscuro \n";
+        cout<<"2- Verde oscuro \n";
+        cout<<"3- Azul claro \n";
+        cout<<"4- Rojo oscuro \n";
+        cout<<"5- Morado \n";
+        cout<<"6- Amarillo \n";
+        cout<<"7- Blanco \n";
+        cout<<"8- Gris oscuro \n";
+        cout<<"9- Azul claro brillante \n";
+        cin>>color;
+        goto reiniciar;
+        }
+
+        if (tecla==67){//F9
+        for (i=0;i=100;i++){
+            pantalla[i][1]=0;
+            pantalla[i][2]=0;
+            pantalla[i][3]=0;
+            pantalla[i][4]=0;
+            pantalla[i][5]=0;
+            pantalla[i][6]=0;
+        }
+        system("cls");
+        return;
+        }
+
+        system("cls");
+        cout<<"Ingrese el tamano de la figura: ";cin>>n;n=n-1;}
+
+    operadorPantalla(tecla,X,Y,n,color);
 
 }
+void colorTexto(int color) {
 
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    switch (color)
+    {
+        case 1: SetConsoleTextAttribute(hConsole, 1);break;
+        case 2: SetConsoleTextAttribute(hConsole, 2);break;
+        case 3: SetConsoleTextAttribute(hConsole, 3);break;
+        case 4: SetConsoleTextAttribute(hConsole, 4);break;
+        case 5: SetConsoleTextAttribute(hConsole, 5);break;
+        case 6: SetConsoleTextAttribute(hConsole, 6);break;
+        case 7: SetConsoleTextAttribute(hConsole, 7);break;
+        case 8: SetConsoleTextAttribute(hConsole, 8);break;
+        case 9: SetConsoleTextAttribute(hConsole, 9);break;
+        default: SetConsoleTextAttribute(hConsole, 7);break;
+    }
+
+
+}
 
 
 
